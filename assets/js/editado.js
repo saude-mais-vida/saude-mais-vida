9271bd643a746bcf01358bd67e36b78b5e125797
fogaofernando <fogaofernando@gmail.com>
2017-10-20 18:09:56 -0200

    adicionando adSence

--- assets/js/editado.js
+++ assets/js/editado.js
@@ -7,14 +7,29 @@ setTimeout(function (){ // tempo para carregar código html
             
             obj.recentes.forEach(function(atual,cont){
 
-                //variaveis/inicialização
+                //guia menu (desktop)        
                 a = document.querySelector(String("#campo"+(cont+1)));
-
-
                 a.setAttribute("href",atual.link);
                 a.innerHTML = atual.nome;
-
+                
+                
+                //guia do escorpo 
+                a = document.querySelector(String("#artTema"+(cont+1)));
+                a.innerHTML = atual.tema;
+                
+                a = document.querySelector(String("#artDetalhes"+(cont+1)));
+                a.innerHTML = atual.detalhes;
+                
+                a = document.querySelector(String("#artLink"+(cont+1)));
+                a.setAttribute("href",atual.link);
+                 
+                a = document.querySelector(String("#artImg"+(cont+1)));
+                a.setAttribute("src",atual.img);
+                
             });
+            
+        
+            
 
         });
 
@@ -26,3 +41,6 @@ setTimeout(function (){ // tempo para carregar código html
 function widthPlataforma(){
     return document.querySelector('body').clientWidth;
 }
+
+
+
